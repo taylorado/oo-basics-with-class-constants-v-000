05195a74754f7596c6cd69fd03e4346a458b95ad
Taylor Donlan <taylordonlan@gmail.com>
2018-03-07 20:49:00 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,36 @@
-class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
+#class Shoe
+#  attr_accessor :color, :size, :material, :condition
+#  attr_reader :brand
 
+#  def initialize(brand)
+#    @brand = brand
+#  end
+
+#  def cobble
+#    self.condition = "new"
+#    puts "Your shoe is as good as new!"
+#  end
+#
+#end
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition 
+  attr_reader :brand  
+ 
+  BRANDS = []
+ 
   def initialize(brand)
     @brand = brand
   end
-
+ 
   def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+      self.condition = "new"
+      puts "Your shoe is as good as new!"
+    end
+ 
+  # create the writer for genre and add the logic for the class constant
+  def brand=(brand)
+    @brand = brand
+    BRANDS << brand 
   end
-
 end
